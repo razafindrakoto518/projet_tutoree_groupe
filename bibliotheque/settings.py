d02b514826f2427b04e2d669b428d96f5e759940
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-04-27 17:55:27 +0300

    Najoutena anaty INSTALLED_APPS le application rehetra

--- bibliotheque/settings.py
+++ bibliotheque/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentification',
+    'tableau_de_bord',
+    'emprunts',
+    'livres',
+    'adherents',
 ]
 
 MIDDLEWARE = [
