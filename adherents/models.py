2721e93aa1d37a3b07c2532acee419ad5ac37f20
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-04-28 10:09:29 +0300

    Nicréer classe Adherent

--- adherents/models.py
+++ adherents/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+FONCTIONS = [
+    ('Etudiant','Etudiant'),
+    ('Formateur', 'Formateur'),
+    ('Employé', 'Employé')
+]
+
+
 # Create your models here.
+class Adherent(models.Model):
+    matricule = models.CharField(primary_key=True)
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=100)
+    email = models.EmailField()
+    fonctions = models.CharField(choices=FONCTIONS)
