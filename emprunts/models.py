142e448705f5f0f05f69d65071ef5fbb2d71417d
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-04-28 10:58:31 +0300

    Ccréation des tables Livre, Adherent et Emprunt

--- emprunts/models.py
+++ emprunts/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from datetime import timedelta, datetime
 
-# Create your models here.
+from django.db import models
+from livres.models import Livre
+from adherents.models import Adherent
+from django.contrib.auth.models import User
+
+STATUT = [
+    ('En retard', 'En retard'),
+    ('Retourné', 'Retourné')
+]
+class Emprunt(models.Model):
+    ref_livre = models.ForeignKey(Livre, on_delete=models.CASCADE)
+    adherent = models.ForeignKey(Adherent, on_delete=models.CASCADE)
+    bibliothecaire = models.OneToOneField(User,on_delete=models.CASCADE)
+    date_emprunt = models.DateField(auto_now_add=True)
+    date_limite = models.DateField(default=datetime.now() + timedelta(days=15))
+    date_retour = models.DateField(null=True)
+    status = models.CharField(choices=STATUT)
+    remarque = models.TextField()
--- livres/models.py
+++ livres/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+CATEGORIE = [
+    ('Math','Mathématique'),
+    ('Physique', 'Physique'),
+    ('Littérature','Littérature'),
+    ('Droit', 'Doit'),
+    ("Memoire de fin d'étude", "Memoire de fin d'étude"),
+    ('Thèse','Thèse')
+]
+
+
+
+class Livre(models.Model):
+    reference = models.CharField(primary_key=True)
+    titre = models.CharField(max_length=150)
+    auteur = models.CharField(max_length=150)
+    categorie = models.CharField(choices=CATEGORIE)
+    quantite = models.PositiveIntegerField(default=1)
+
+
+    def __str__(self):
+        return f"{self.reference} - {self.titre}"
