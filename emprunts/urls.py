470bcf7dc64cc8ce2f581b0efbbd083d284fbb0b
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-04-29 06:28:43 +0300

    Modification url

--- emprunts/urls.py
+++ emprunts/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.liste_emprunt, name= "liste_emprunt"  ),#créer 
+    path('',views.liste_emprunt, name= "listeEmprunt"  ),#créer 
 ]
