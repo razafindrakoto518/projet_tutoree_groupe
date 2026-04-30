93647fd4da247d6bf30650b628b30d0c2df8152c
minifi <niriantsoamiantsafifaliana@gmail.com>
2026-04-30 12:59:42 +0300

    nanao an'le form corriger

--- emprunts/forms.py
+++ emprunts/forms.py
@@ -3,32 +3,5 @@ from django import forms
 from .models import Emprunt
 class EmpruntForm(forms.ModelForm):
     class Meta :
-        model = Emprunt
-        fields =['ref_livre', 'adherent','date_retour','date_limite','statut']
-        widget ={
-            'ref_livre': forms.TextInput(attrs={
-                'class':'input',
-                'placeholder':'Livre'
-            }),
-            'adherent': forms.TextInput(attrs={
-                'class':'input',
-                'placeholder':'Empruteur'
-            }),
-            'date_emprunt': forms.TextInput(attrs={
-                'class':'input',
-                'placeholder':'Date Emprunt'
-            }),
-            'date_retour': forms.TextInput(attrs={
-                'class':'input',
-                'placeholder':'Date Retour'
-            }),
-            'date_limite': forms.TextInput(attrs={
-                'class':'input',
-                'placeholder':'Date Limite'
-            }),
-            'statut': forms.TextInput(attrs={
-                'class':'input',
-                'placeholder':'Statut'
-            }),
-
-        }
+        model=Emprunt
+        fields=["ref_livre","adherent","date_emprunt","date_limite","date-retour"]
