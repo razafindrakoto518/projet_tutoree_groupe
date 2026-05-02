1c2075f5ac8f05a448d948a20dc1baa1337591e6
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-02 19:40:57 +0300

    Ajout url pour ajouter un emprunt

--- emprunts/urls.py
+++ emprunts/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.liste_emprunt, name= "listeEmprunt"  ),#créer 
+    path('',views.liste_emprunt, name= "listeEmprunt"  ),
+    path('ajouter/', views.enregistrer_emprunt, name='enregistrerEmprunt') 
 ]
