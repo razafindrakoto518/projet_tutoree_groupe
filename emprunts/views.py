414555b07b06b8c8d4e592d1bd32983b4c836c4f
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-02 19:38:37 +0300

    Ajout fonction pour gerer l'enregistrement de emprunts

--- emprunts/views.py
+++ emprunts/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
-from .models import Emprunt
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
 
+from livres.models import Livre
+from .models import Emprunt
+from .forms import EmpruntForm
+
+@login_required
 def liste_emprunt(request):
-    emprunts = Emprunt.objects.all()
-    return render (request, 'emprunt/list.html' ,{"emprunts" : emprunts })
+    emprunts = Emprunt.objects.all().order_by('-date_emprunt')[:10]#On limite la liste à dix
+    return render (request, 'emprunts/list.html' ,{"emprunts" : emprunts })
+
+
+#Fonction pour enregistrer un emprunt
+@login_required
+def enregistrer_emprunt(request):
+    #Lors de la soumission du formulaire
+    if request.method == "POST":
+        form = EmpruntForm(request.POST)
+        if form.is_valid():
+            #On enrregistre pas directement les données , on les stocke dans une variable emprunt
+            emprunt = form.save(commit=False)
+            #on recupere l'objet livre correspondat au réference selectionné
+            livre = get_object_or_404(Livre, reference = emprunt.ref_livre.reference)
+            #On vérifie que il y a encore au mois une livre en stock
+            if livre.quantite < 1:#Le stock est vide quantite = 0
+                form = EmpruntForm()
+                return render(request, "emprunts/ajouter_emprunt.html", {
+                'message' : 'Cette livre n\'est plus disponible',#Message d'erreur à affiché au utilisateur
+                'form' : form 
+                })
+            else:#Si il y au mois une livre en stock
+                emprunt.bibliothecaire = request.user#On remplie le champ bibliothecaire par l'utilisateur connecté
+                emprunt.save()#on enregistre définitivement
+
+                livre.quantite -= 1#On soustrait 1 la quantite en stock du livre
+                livre.save()#on enregistre le changement
+
+                return redirect('listeEmprunt')#Rédiriger vers la page liste des emprunts
+        else:
+            return HttpResponse("Formulaire invalide")
+    else:
+        form = EmpruntForm()
+        return render(request, "emprunts/ajouter_emprunt.html", {
+            'form' : form
+        })
