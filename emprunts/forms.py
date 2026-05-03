60e3e48bf5d522b75fc4918e923725917a10b422
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-03 18:01:42 +0300

    Création d'un formulaire pour retour emprunt

--- emprunts/forms.py
+++ emprunts/forms.py
@@ -4,3 +4,17 @@ class EmpruntForm(forms.ModelForm):
     class Meta :
         model = Emprunt
         fields= ['ref_livre','adherent']
+
+
+class EnregistrementRetourEmprunt(forms.ModelForm):
+    class Meta:
+        model = Emprunt
+        fields = ['date_retour','remarque']
+        widgets = {
+            'date_retour' : forms.DateInput(attrs={
+                'class' : 'form-control'
+            }),
+            'remarque' : forms.Textarea(attrs={
+                'class' : 'form-control'
+            })
+        }
