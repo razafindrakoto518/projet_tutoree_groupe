2e444fa9f0d206741443c2084eb9dae3f2dae5d7
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-03 18:00:03 +0300

    Ajout url pour enregistrement retour emmrunt

--- emprunts/urls.py
+++ emprunts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.liste_emprunt, name= "listeEmprunt"  ),
-    path('ajouter/', views.enregistrer_emprunt, name='enregistrerEmprunt') 
+    path('ajouter/', views.enregistrer_emprunt, name='enregistrerEmprunt'),
+    path('retourner/<int:id>/', views.retourner_emprunt, name='retournerEmprunt') 
 ]
